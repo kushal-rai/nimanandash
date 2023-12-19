2c3e7cefe3ca188646b7fb8c7f014f510439fb2f
Kushal <rkushal258@gmail.com>
2023-12-20 00:09:05 +1100

    Configure React app for deployment to GitHub

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import AppLayout from "./pages/AppLayout";
 import GoogleForm from "./pages/GoogleForm";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 
 function App() {
   return (
